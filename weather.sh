8d5af2bd842293cf5d547e4fbfb9a04e549f91d0
Matvei <matvei@ubuntu.ubuntu-domain>
2014-03-05 17:52:17 +0300

    value changed to variable

--- weather.sh
+++ weather.sh
@@ -2,7 +2,7 @@
 . $PWD/weather.conf
 while true; do
 	wget -t 1 -O $TEMP_FILE -o $TEMP_LOG $SOURCE
-	grep -Po '(?<=<div class="b-thermometer__now">)([^</div>]*)' index.html | awk '{print $1}' 
+	grep -Po '(?<=<div class="b-thermometer__now">)([^</div>]*)' $TEMP_FILE | awk '{print $1}' 
 	rm $TEMP_FILE
 	rm $TEMP_LOG
 	sleep $DELAY_TIME
